e956bf16b000467faa26383b766aa9d00dc03377
Leah Boyd <leahboyd08@gmail.com>
2019-03-18 17:39:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,8 @@
 def square_array(array)
-  array = [1,2,3,4,5,6,7,8,9,10]
-  array.each_with_index
+  array = []
+  array.each { |index| array << index ** 2}
+  array
 end
+
+my_array = [1, 2, 3, 4]
+puts square_array(my_array)
