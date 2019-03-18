aef85059f8803de34bd4173966bdc26dcfe51b2a
Leah Boyd <leahboyd08@gmail.com>
2019-03-18 17:34:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
 def square_array(array)
   array = [1,2,3,4,5,6,7,8,9,10]
-  array.each_with_index {|value| puts "array[#{value}] ** 2 = #{array[sum]}"}
+  array.each_with_index
+  puts "array[#{value}] ** 2 = #{array[sum]}"
 end
